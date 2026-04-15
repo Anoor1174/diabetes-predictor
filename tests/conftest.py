31e466ec00ea6bba57113436b3c3583a513461f9
anoor71 <azhaarnoor121@gmail.com>
2026-04-15 15:24:15 +0100

    Fixed Chart.js

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,10 @@
+import sys
+import os
 import pytest
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, PROJECT_ROOT)
+
 from app.app import app
 
 @pytest.fixture
@@ -6,3 +12,4 @@ def client():
     app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
+
